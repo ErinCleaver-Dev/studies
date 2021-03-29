a2d6a2f1d26cd99ffb92f5f1feabba6638ee7799
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2021-03-29 18:10:59 -0400

    3-29-2021

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import styled from 'styled-components'
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home'
 import Algorithms from './Components/Algorithms/Algorithms'
-import DataStructures from './components/Data Structures/DataStructures'
+import DataStructures from './Components/DataStructures/DataStructures'
+import HomeWork from './Components/Homework/Homework'
 import Notes from './Components/Notes/Notes'
 import Projects from './Components/Projects/Projects'
 import Resume from './Components/Resume/Resume'
@@ -27,6 +26,9 @@ function App() {
           <Route path="/DataStructures">
             <DataStructures/>
           </Route>
+          <Route path="/HomeWork">
+            <HomeWork/>
+          </Route>
           <Route path="/Notes">
             <Notes/>
           </Route>
@@ -46,5 +48,4 @@ function App() {
 export default App;
 
 const Container = styled.div`
-
 `
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import styled from 'styled-components';
-
+import {Link} from 'react-router-dom'
 function header() {
     return (
-        <div>
-            
-        </div>
+        <Constructor>
+            <HomePage>
+                <Link to="/">
+                    Home
+                </Link>
+                <Link to="">
+                    
+                </Link>
+            </HomePage>
+        </Constructor>
     )
 }
 
 export default header
+const Constructor = styled.div`
+    background-color: #17332f;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+    height: 50px;
+`
+const HomePage = styled.div`
+    a {
+        color: white;
+        text-decoration: none;
+    }
+`
+
