19a4d090c77e966bad6c12b41fed607a4e1fb66d
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-03-30 07:33:04 -0400

    update 3-30-21 at 731
    Added information to the home page.  A introduction to my self and a picture of my self.

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,6 +8,13 @@ function Home() {
             <Helmet>
                 <title>Home</title>
             </Helmet>
+            <Introduction>
+                    <p>Hello, My name is Erin</p>
+                    <p>I am currently studying react in hopes of finding a job working with the lanague.</p>
+            </Introduction>
+            <Picture>
+                <img src="https://i.imgur.com/pv9TKMx.jpg"></img>
+            </Picture>
         </Container>
     )
 }
@@ -17,7 +24,27 @@ export default Home
 
 const Container = styled.div`
     display: flex;
-    flex-direction: column;
     background-color: #ccc5b1;
+    justify-content: center;
     height: 700px;
 `
+
+const Introduction = styled.div`
+    margin-top: 20px;
+    color: black;
+    height: 200px;
+    width: 70vh;
+    background-color: white;
+    border-radius: 20px;
+    padding-left: 20px;
+    margin-right:  20px;
+`
+
+const Picture = styled.div`
+    margin-top: 20px;
+    height: 200px;
+    img {
+        height: 100%;
+        border-radius: 20px;
+    }
+`;
